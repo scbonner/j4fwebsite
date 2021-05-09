38d38e48e7eddf5f235cf21a8ece558c6021c0bf
scbonner <45452011+scbonner@users.noreply.github.com>
2021-05-08 21:40:31 -0400

    experiencing problems linking App.css to logo file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import Logo from './components/Logo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Logo from '../src/components/Logo';
 import './App.css';
 
 function App() {
   return (
+//rendering logo component
     <div className="App">
-    <Logo />
-      Chasing criminals
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <Logo /> 
     </div>
   );
 }
